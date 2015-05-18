93ac10433048704839519e46ded92e7fe0ede415
JakubKoziara <jakub.koziara1@gmail.com>
2015-05-18 11:55:04 +0000

    ModelTickets

--- Projekt/db/schema.rb
+++ Projekt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511110429) do
+ActiveRecord::Schema.define(version: 20150512100708) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,17 @@ ActiveRecord::Schema.define(version: 20150511110429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "status"
+    t.string   "difficulty"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tickets", ["article_id"], name: "index_tickets_on_article_id"
 
 end
