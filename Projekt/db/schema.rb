992cb1dd075b1e7b83eaa76d0d3737311aa70520
JakubKoziara <jakub.koziara1@gmail.com>
2015-06-01 14:28:35 +0000

    Added field to tickets

--- Projekt/db/schema.rb
+++ Projekt/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601114130) do
+ActiveRecord::Schema.define(version: 20150601142359) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150601114130) do
     t.integer  "article_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "priority"
   end
 
   add_index "tickets", ["article_id"], name: "index_tickets_on_article_id"
